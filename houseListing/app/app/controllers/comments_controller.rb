804189a4a91b66b14a38b3f3adc64a66b9371833
Matthew Kachlik <kachlikmatt@gmail.com>
2015-04-10 21:23:59 -0500

    added price as a float variable
    added price as a float variable

---
+++ houseListing/app/app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  def create
+    @listing = Listing.find(params[:listing_id])
+    @comment = @listing.comments.create(comment_params)
+    redirect_to listing_path(@listing)
+  end
+def destroy
+    @listing = Listing.find(params[:listing_id])
+    @comment = @listing.comments.find(params[:id])
+    @comment.destroy
+    redirect_to listing_path(@listing)
+  end
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
--- houseListing/app/app/controllers/listings_controller.rb
+++ houseListing/app/app/controllers/listings_controller.rb
@@ -1,24 +1,21 @@
-
 class ListingsController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   def index
     @listings = Listing.all
   end
+
   def show
     @listing = Listing.find(params[:id])
   end
+
   def new
     @listing = Listing.new
   end
+
   def edit
     @listing = Listing.find(params[:id])
   end
-  def destroy
-    @listing = Listing.find(params[:id])
-    @listing.destroy
 
-    redirect_to listings_path
-  end
   def create
     @listing = Listing.new(listing_params)
 
@@ -28,6 +25,7 @@ class ListingsController < ApplicationController
       render 'new'
     end
   end
+
   def update
     @listing = Listing.find(params[:id])
 
@@ -37,8 +35,15 @@ class ListingsController < ApplicationController
       render 'edit'
     end
   end
-  private
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+
+    redirect_to listings_path
+  end
+ private
   def listing_params
-    params.require(:listing).permit(:address,:price,:description,:time_stamp)
+    params.require(:listing).permit(:address, :price, :time_stamp :description)
   end
 end
