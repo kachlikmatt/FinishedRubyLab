2e1e5107f6a1b4a5769ab18cc70db0e13e03d4c2
Matthew Kachlik <kachlikmatt@gmail.com>
2015-04-17 22:13:02 -0500

    adding users

--- houseListing/db/schema.rb
+++ houseListing/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414033937) do
+ActiveRecord::Schema.define(version: 20150415020923) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter",  limit: 255
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20150414033937) do
     t.datetime "time_stamp"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.string   "password",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_foreign_key "comments", "listings"
